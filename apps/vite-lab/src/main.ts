461e8e4c29cd290efed62cd3b5305fb89270a491
covik <6125218+Covik@users.noreply.github.com>
2022-11-05 00:47:24 +0100

    feat(vite-lab): add basic vue support

--- apps/vite-lab/src/main.ts
+++ apps/vite-lab/src/main.ts
@@ -1,9 +1,5 @@
-import './style.css';
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const app = document.querySelector<HTMLDivElement>('#app')!;
-
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+createApp(App).mount('#app')
