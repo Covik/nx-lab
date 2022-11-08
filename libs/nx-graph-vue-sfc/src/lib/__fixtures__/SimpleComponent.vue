ffd23231c74d866762a012234bab23941d4cb5f8
covik <6125218+Covik@users.noreply.github.com>
2022-11-08 02:10:44 +0100

    test(graph): explore imports parsed by sfc-compiler

---
+++ libs/nx-graph-vue-sfc/src/lib/__fixtures__/SimpleComponent.vue
@@ -0,0 +1,7 @@
+<script>
+import { apiInterface } from '@nx-lab/api-interface'
+
+export default {
+  name: 'SimpleComponent',
+}
+</script>
--- libs/nx-graph-vue-sfc/src/lib/nx-graph-vue-sfc.spec.ts
+++ libs/nx-graph-vue-sfc/src/lib/nx-graph-vue-sfc.spec.ts
@@ -1,7 +1,38 @@
-import { nxGraphVueSfc } from './nx-graph-vue-sfc';
+import * as compiler from 'vue/compiler-sfc'
+import * as fs from 'fs'
+import * as path from 'path'
 
-describe('nxGraphVueSfc', () => {
-  it('should work', () => {
-    expect(nxGraphVueSfc()).toEqual('nx-graph-vue-sfc');
-  });
-});
+const simpleComponent = fs.readFileSync(path.resolve(__dirname, './__fixtures__/SimpleComponent.vue'), { encoding: 'utf-8' })
+const projectLibrary = '@nx-lab/api-interface'
+
+describe('@vue/sfc-compiler exploration', () => {
+  test(`script compiler should find ${projectLibrary}`, () => {
+    const importDeclarations = extractImportDeclarations(simpleComponent)
+    const moduleNames = findModulesNames(importDeclarations)
+
+    expect(moduleNames).toContain(projectLibrary)
+  })
+})
+
+function findModulesNames(declarations: unknown[]) {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  return declarations.map((declaration) => declaration.source.value)
+}
+
+function extractImportDeclarations(source: string) {
+  const noSourceMaps =  { sourceMap: false }
+
+  const { errors, descriptor } = compiler.parse(source, noSourceMaps)
+  if (errors.length !== 0) {
+    throw new Error(`Failed to parse component. Errors: ${JSON.stringify(errors, undefined, 2)}`, )
+  }
+
+  const script = compiler.compileScript(descriptor, {
+    id: 'irrelevant',
+    reactivityTransform: false,
+    ...noSourceMaps,
+  })
+  const ast = script.scriptAst ?? []
+  return ast.filter(({type}) => type === 'ImportDeclaration')
+}
